5374302afa1fa2f99ef8363605f8c0dafd4e8f86
Christopher <illeww@hotmail.com>
2023-01-21 13:54:34 -0500

    Pushing_to_Github

---
+++ frontend-tech-sales/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <a href="/"> Simon Tech </a>
+      </header>
+      <main>Product List</main>
+    </div>
+  );
+}
+
+export default App;
